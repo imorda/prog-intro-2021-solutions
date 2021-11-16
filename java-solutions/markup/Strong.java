bcecb24d8a4b00d95ecafd23ec6d09b53ae0d303
Georgiy Korneev <kgeorgiy@kgeorgiy.info>
2021-11-16 17:11:34 +0300

    Commented

--- java-solutions/markup/Strong.java
+++ java-solutions/markup/Strong.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 public class Strong extends AbstractMarkupElement {
     private static final Tag TAG = new Tag("__", "b");
+
     public Strong(List<MarkupCombinable> content) {
         super(content, TAG);
     }
+
     public Strong(MarkupCombinable content) {
         super(content, TAG);
     }
--- java-solutions/markup/UnorderedList.java
+++ java-solutions/markup/UnorderedList.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 public class UnorderedList extends AbstractMarkupList {
     private static final Tag TAG = new Tag("", "list");
+    
     public UnorderedList(List<ListItem> content) {
         super(content, TAG);
     }
+
     public UnorderedList(ListItem content) {
         super(content, TAG);
     }
